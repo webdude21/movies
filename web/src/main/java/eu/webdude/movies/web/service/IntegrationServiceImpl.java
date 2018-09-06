72644e0e35798ddae2a525a58aee8b6cdcca2690
webdude21 <dimo.petrof@gmail.com>
2018-09-06 21:01:14 +0300

    configure logging

--- web/src/main/java/eu/webdude/movies/web/service/IntegrationServiceImpl.java
+++ web/src/main/java/eu/webdude/movies/web/service/IntegrationServiceImpl.java
@@ -1,13 +1,18 @@
 package eu.webdude.movies.web.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class IntegrationServiceImpl implements IntegrationService {
 
 	private final JmsTemplate jmsTemplate;
+
+	private static final Logger logger = LoggerFactory.getLogger(IntegrationServiceImpl.class);
 
 	@Autowired
 	public IntegrationServiceImpl(JmsTemplate jmsTemplate) {
@@ -16,6 +21,7 @@ public class IntegrationServiceImpl implements IntegrationService {
 
 	@Override
 	public void importData() {
+		logger.debug("Sending the import data start command!");
 		jmsTemplate.convertAndSend("");
 	}
 }
